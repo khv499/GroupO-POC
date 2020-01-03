0ceb56fda7c8193f427d353cbfdcfffa705476dc
harsha.koneru@valuelabs.com <harsha.koneru@T03.ads.valuelabs.net>
2020-01-03 15:45:37 +0530

    Added

---
+++ Frontend/js/config.js
@@ -0,0 +1,14 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_4BmYEkobE',
+        region: 'us-east-1',
+        clientId: 'bcgto49u0iskhm5vtme4am35h'
+    },
+    urls: {
+        portfolioURL: 'https://khv499.github.io/Sample-POC/Frontend/app/index.html',
+        signInURL: 'https://khv499.github.io/Sample-POC/Frontend/signIn.html',
+        signUpURL: 'https://khv499.github.io/Sample-POC/Frontend/signUp.html',
+        forgotPasswordURL: 'https://khv499.github.io/Sample-POC/Frontend/forgotPassword.html',
+        confirmForgotPasswordURL: 'https://khv499.github.io/Sample-POC/Frontend/confirmForgotPassword.html'
+    }   
+}
